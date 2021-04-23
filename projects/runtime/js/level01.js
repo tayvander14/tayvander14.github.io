81d225f44d9256d46b5f113a034b45d2e8ca7148
tayvander14 <taylorleora14@gmail.com>
2021-04-23 17:00:08 +0000

    Update

--- projects/runtime/js/level01.js
+++ projects/runtime/js/level01.js
@@ -40,7 +40,7 @@ var level01 = function (window) {
         };
         window.levelData = levelData;
         // set this to true or false depending on if you want to see hitzones
-        game.setDebugMode(true);
+        game.setDebugMode(false);
 
         // TODO 6 and on go here
         // BEGIN EDITING YOUR CODE HERE
